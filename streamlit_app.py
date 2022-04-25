6b1fbd0fc01a11a0ae94b55eece62cda00c46e81
trjade1234 <104291460+trjade1234@users.noreply.github.com>
2022-04-25 08:32:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import streamlit as st
 from gsheetsdb import connect
 from google.oauth2 import service_account
 
-st.title('This is Eco-score!')
+#st.title('This is Eco-score!')
+st.title("生态驾驶评分，助力您的绿色驾驶新体验")
 
 #if st.sidebar.button('Back to home'):
 #    st.write("  ")
@@ -23,18 +24,22 @@ st.write(df.head())
 
         
 #select a specific vehicle
-st.subheader('Choose one vehicle to visualize')
+#st.subheader('Choose one vehicle to visualize')
+st.subheader("您想查看哪辆车")
 vehoption = st.selectbox('', df.vehid.unique())
 vehselected = df[df.vehid == vehoption]
 
 #select a spcific trip of that vehicle
-st.write('This vehicle has in total ', str(len(vehselected.orderid.unique())), ' trips')
+#st.write('This vehicle has in total ', str(len(vehselected.orderid.unique())), ' trips')
+st.write('这辆车共有',str(len(vehselected.orderid.unique())), '条行程')
 
-st.subheader('Choose one trip to visualize')
+#st.subheader('Choose one trip to visualize')
+st.subheader("您想查看哪趟行程")
 tripoption = st.selectbox('', vehselected.orderid.unique())
 tripselected = vehselected[vehselected.orderid == tripoption]
 
-st.subheader('This is the trajectory')
+#st.subheader('This is the trajectory')
+st.subheader("行程路线图")
 map_data = pd.DataFrame(columns = ['lat', 'lon'])
 map_data['lat'] = tripselected.latitude
 map_data['lon'] = tripselected.longitude
