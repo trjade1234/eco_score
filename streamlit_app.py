cdf51fed0abf0572c474cca451dd299e4776afe8
trjade1234 <104291460+trjade1234@users.noreply.github.com>
2022-04-25 08:57:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 from gsheetsdb import connect
-from google.oauth2 import service_account
-import geopy
+from google.oauth2 import service_account3
 
 #st.title('This is Eco-score!')
 st.success.title("生态驾驶，您的绿色驾驶新体验")
