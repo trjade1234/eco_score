d2c6149314cf6a370676eb942d973d51f69824db
trjade1234 <104291460+trjade1234@users.noreply.github.com>
2022-04-25 11:00:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,16 +30,16 @@ vehid = df.vehid.unique()
 #select a specific vehicle
 #st.subheader('Choose one vehicle to visualize')
 st.success("您想查看哪辆车")
-vehoption = st.selectbox('', df.vehid.unique())
+vehoption = st.selectbox('选择车辆编号', df.vehid.unique())
 vehselected = df[df.vehid == vehoption]
 
 #select a spcific trip of that vehicle
 #st.write('This vehicle has in total ', str(len(vehselected.orderid.unique())), ' trips')
-st.markdown(str('这辆车共有'+str(len(vehselected.orderid.unique()))+'条行程'))
+#st.markdown(str('这辆车共有'+str(len(vehselected.orderid.unique()))+'条行程'))
 
 #st.subheader('Choose one trip to visualize')
 st.success("您想查看哪趟行程")
-tripoption = st.selectbox('', vehselected.orderid.unique())
+tripoption = st.selectbox('选择行程编号', vehselected.orderid.unique())
 tripselected = vehselected[vehselected.orderid == tripoption]
 #st.write(tripselected.head())
 
@@ -51,15 +51,15 @@ map_data = pd.DataFrame(columns = ['lat', 'lon'])
 map_data['lat'] = tripselected.latitude
 map_data['lon'] = tripselected.longitude
 with col1:
-        st.subheader("行程路线图")
+        st.success("行程路线图")
         st.map(map_data)
 
 scores = 70
 with col2:
-        st.subheader('评分状况')
+        st.success('评分状况')
 if st.sidebar.button('查看您的驾驶评分'):
         with col2:
-                st.subheader('评分状况')
+                st.subsubheader('总评分状况')
                 st.write(str('总评分'+str(scores)))
                 my_bar = st.progress(0)
                 my_bar.progress(scores + 1) #this has to be changed
@@ -69,14 +69,14 @@ if st.sidebar.button('驾驶轨迹分项分析'):
         avgacc = tripselected['acceleration'].mean()
         idleperc = 0.1
         with col2:
-                st.subheader('评分状况')
+                st.subsubheader('分项评分状况')
                 st.write(str('平均速度'+str(round(avgspeed,2))+'km/h'+' 排名前50%'))
                 avgspeed_bar = st.progress(0)
                 avgspeed_bar.progress(50 + 1) #this has to be changed
-                st.subheader(str('平均加速度'+str(round(avgacc,2))+'m/s2'+' 排名前50%'))
+                st.write(str('平均加速度'+str(round(avgacc,2))+'m/s2'+' 排名前50%'))
                 avgacc_bar = st.progress(0)
                 avgacc_bar.progress(60 + 1) #this has to be changed
-                st.subheader(str('怠速比例'+str(idleperc*100)+'%'+' 排名前80%'))
+                st.write(str('怠速比例'+str(idleperc*100)+'%'+' 排名前80%'))
                 idleperc_bar = st.progress(0)
                 idleperc_bar.progress(80 + 1) #this has to be changed
 
